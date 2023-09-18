a9e78dcb1c2df9f9670ae8d6b0e98bcde9eb5722
Your Name <you@example.com>
2023-09-18 16:01:57 +0200

    creation of guessing a number game

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 'use strict';
-/*
-console.log(document.querySelector('p.message').textContent);
-
-document.querySelector('.message').textContent = '🎉Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 15;
-
-console.log(document.querySelector ('.guess').value)
-document.querySelector ('.guess').value = 23;
-console.log(document.querySelector ('.guess').value)
-*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
@@ -36,25 +24,11 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.highscore').textContent = highScore;
         }
     }
-    
-// when guess is too high
-    else if(guess > secretNumber){
-        if(score > 1){
-            document.querySelector('.message').textContent = 'Too Higher';
-            score--;
-            document.querySelector('.score').textContent = score;
-        }else{
-            document.querySelector('.message').textContent = 'You lost the Game';
-            document.querySelector('.score').textContent = 0;
-        }
-        
-    }
-    
-// when guess is too low
-    else if(guess < secretNumber){
 
+ // when the guess is wrong
+    else if(guess !== secretNumber){
         if(score > 1){
-            document.querySelector('.message').textContent = 'Too Low';
+            document.querySelector('.message').textContent = guess > secretNumber ? 'Too Higher' : 'Too Low';
             score--;
             document.querySelector('.score').textContent = score;
         }else{
